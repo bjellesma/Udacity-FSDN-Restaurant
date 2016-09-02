6822e801866fe21b3447cde4fcfebd13185a84a0
bjellesma <william.jellesma@gmail.com>
2016-09-01 23:31:56 -0400

    feat:webserver can create, read, update. TODO: need delete

--- vagrant/resturant/webserver.py
+++ vagrant/resturant/webserver.py
@@ -2,34 +2,113 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 #cgi is common gateway interface
 import cgi
 
+"""
+The follow is all the information for sqlalchemy
+"""
+from sqlalchemy import create_engine
+#we need sessionmaker in order to perform queries
+from sqlalchemy.orm import sessionmaker
+#importing previously made files
+from database_setup import Base, Restaurant, MenuItem
+#creating sqlite3 engine
+engine = create_engine('sqlite:///restaurantmenu.db')
+#connecting to db file
+Base.metadata.bind = engine
+#starting session with engine
+DBSession = sessionmaker(bind = engine)
+#creating instance of DBsession
+session = DBSession()
+"""
+"""
+
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path.endswith("/hello"):
+        #splits path by ? so that url parameters are handled
+        p = self.path.split("?")
+        path = p[0]
+        URLparams = {}
+        if len(p) > 1:
+            URLparams = p[1].split("&")
+        if path == "/restaurants":
             #status code 200 indicates successful get request
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
+            #create the restaurants to contain all of the restaurants in the db
+            restaurants = session.query(Restaurant).all()
+
             message = ""
-            message += "<html><body>Hello!</body></html>"
-            message += "<form method='Post' enctype='multipart/form-data' action='/hello'><h2>What do you want to post to the server?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"
+            message += "<html><title>Restaurants</title><body>"
+            message += "<p>If you'd like, you may create a <a href='/restaurants/new'>new</a> Restaurant</p>"
+            for restaurant in restaurants:
+                message += "<br>"
+                message += "<br>"
+                message += "Restaurant Name: %s" % restaurant.name
+                message += "<br>"
+                message += "<a href='/restaurants/edit?%s'>Edit</a>" % restaurant.id
+                message += "<br>"
+                message += "<a href='/restaurants/delete?%s'>Delete</a>" % restaurant.id
+                message += "<br>"
+                message += "<br>"
             self.wfile.write(message)
-            #used for debugging
-            print message
             return
-        elif self.path.endswith("/hola"):
-            #status code 200 indicates successful get request
+        elif self.path == "/restaurants/new":
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
+            #create the restaurants to contain all of the restaurants in the db
+            restaurants = session.query(Restaurant).all()
+
             message = ""
-            message += "<html><body>Hola Mi Amigo</body></html>"
-            message += "<form method='Post' enctype='multipart/form-data' action='/hello'><h2>What do you want to post to the server?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form></body></html>"
+            message += "<html><title>Create</title><body>"
+            message += '''<form method='Post' enctype='multipart/form-data' action='/restaurants/new'>
+            <h2>What would you like the name of the new Restaurant to be?</h2>
+            <input name='name' type='text' >
+            <input type='submit' value='Submit'>
+            </form>
+            '''
+
             self.wfile.write(message)
-            #used for debugging
-            print message
+            return
+        elif path == "/restaurants/edit":
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            #so python doesn't complain
+            name = ''
+
+            if URLparams:
+                if len(URLparams) == 1:
+                    id = URLparams[0]
+                elif len(URLparams) == 2:
+                    idPair = URLparams[0].split('=')
+                    id = idPair[1]
+                    namePair = URLparams[1].split('=')
+                    name = namePair[1]
+                else:
+                    self.send_error(403, 'Not Accessible: %s' % self.path)
+            #create the restaurants to contain all of the restaurants in the db
+            restaurant = session.query(Restaurant).filter_by(id = id).one()
+            if name:
+                restaurant.name = name
+                session.add(restaurant)
+                session.commit()
+            message = ""
+            message += "<html><title>Update</title><body>"
+            message += '''<form method='get' enctype='multipart/form-data' action='/restaurants/edit'>
+            <h2>What would you like the name of the new Restaurant to be now?</h2>
+            <input name='id' type='hidden' value='%s'>
+            <input name='name' type='text' >
+            <input type='submit' value='Submit'>
+            </form>
+            ''' % id
+
+            self.wfile.write(message)
             return
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -49,12 +128,17 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 #use the fields variable to parse out the dictionary of parameters that we've sent
                 #messagecontent will be an array
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                messagecontent = fields.get('name')
             output = ""
             output += "<html><body>"
-            output += " <h2>Here is the form data: </h2"
+            output += " <h2>Here is the name of your restaurant: </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
-            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What do you want to post to the server?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"
+            output += "Go back to <a href='/restaurants'>restaurants</a> to view it"
+            restaurant = Restaurant(name = messagecontent[0])
+            #addming new item to staging area
+            session.add(restaurant)
+            #commiting changes to database
+            session.commit()
             self.wfile.write(output)
             print output
         except:
